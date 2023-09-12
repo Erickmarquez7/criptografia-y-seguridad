c10d3890a99bb8cd20a6a6cf0f6c54d5e72fa86d
Erick Bernal M <erick07@ciencias.unam.mx>
2023-09-11 23:05:15 -0600

    fin p3

--- practicas/practica-3/descifrador.py
+++ practicas/practica-3/descifrador.py
@@ -1,9 +1,9 @@
 from textwrap import wrap
 
 '''Lee el archivo en bytes y nos regresa la lista en numeros base 10'''
-def leer_archivo():
-    archivo_lol = input("Dime le nombre del archivo: ")
-    archivo_lol="archivos-cifrados/"+archivo_lol
+def leer_archivo(archivo):
+    #archivo_lol = input("Dime le nombre del archivo: ")
+    archivo_lol="archivos-cifrados/"+archivo
     #pasamos el archivo en modo lectura por byte
     archivo = open(archivo_lol, 'rb').read()
     #lo pasamos a lista de numeros para que sea mas facil manipular
@@ -47,39 +47,38 @@ def base_64(lista_cif):
     binarios = []
     for n in lista_cif:
         binarios.append(bin(n)[2:])
-
+    #rellenamos con 0 a la izquierda para tener 6 bits
     binarios_p=[]
     for b in binarios:
         b = b.zfill(6)
         binarios_p.append(b)
-
+    # lo transformamos a una cadenota
     ceros_unos = ''
     for b in binarios_p:
         ceros_unos+=str(b)
-
+    #los agrupamos de 8 en 8
     ceros_unos_8=wrap(ceros_unos,8)
-
+    #una vez agrupados los pasamos a su valor decimal
     hexa = []
     for n in ceros_unos_8:
         hexa.append(int(n,2))
-    
+    #quitamos el penultimo para que funciones
     hexa.pop(-2)
-
     g = bytes(list(hexa))
-
     return g
 
 # leemos y desciframos el archivo 1 con cesar
-#lista_cif_cesar = leer_archivo()
-#lista_des_cesar = cesar(lista_cif_cesar)
-#escribir_archivo(lista_des_cesar, 'file1.mp4')
+lista_cif_cesar = leer_archivo('file1.lol')
+lista_des_cesar = cesar(lista_cif_cesar)
+escribir_archivo(lista_des_cesar, 'file1.mp4')
 
 
 # leemos y desciframos el archivo 2 con base 64
-lista_cif_base = leer_archivo()
+lista_cif_base = leer_archivo('file2.lol')
 lista_des_base = base_64(lista_cif_base)
 escribir_archivo(lista_des_base, 'file2.jpg')
 
-#lista_cif_afin = leer_archivo()
-#lista_des_afin = afin(lista_cif_afin)
-#escribir_archivo(lista_des_afin, 'file3.mp3')
+# leemos y desciframos el archivo 3 con afin
+lista_cif_afin = leer_archivo('file3.lol')
+lista_des_afin = afin(lista_cif_afin)
+escribir_archivo(lista_des_afin, 'file3.mp3')
